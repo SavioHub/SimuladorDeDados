420987099f120a9e1cfc4676926ffbde0da76f3f
SavioHub <saviosantossilva19@gmail.com>
2021-06-14 20:40:32 -0300

    PySimpleGUI

--- Simulador.py
+++ Simulador.py
@@ -2,21 +2,36 @@
 # simular o uso de um dado, gerando um valor de 1 até 6
 
 import random
+import PySimpleGUI as sg
 # _init_self para iniciar o projeto
 
 
 class SimuladorDeDado:
+
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'vc gostaria de gerar um novo valor para o dado?'
+        # self.mensagem = 'vc gostaria de gerar um novo valor para o dado?'
+
+        # layout
+    layout = [
+        [sg.Text("Jogar dado?")],
+        [sg.Button('sim'), sg.Button('não')]
+    ]
 
     def iniciar(self):
+        # criar uma janeja
+        self.janela = sg.Window('Simulador de Dado', layout=self.layout)
+        # ler os valores da tela
+        self.eventos, self.valores = self.janela.read()
+        # fazer alguma c oisa com esses valores
+
+        # resposta = input(self.mensagem).lower()
         try:
-            resposta = input(self.mensagem).lower()
-            if resposta == 'sim' or resposta == 's':
+            # self.eventos == 'sim':
+            if self.eventos == 'sim':
                 self.GerarValorDado()
-            elif resposta == 'nao' or resposta == 'n':
+            elif self.eventos == 'não':
                 print('Obrigado pela sua participação.')
             else:
                 print('Apenas: sim ou não')
---
+++ teste.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+
+# Define the window's contents
+layout = [
+    [sg.Text("Jogar dado?")],
+    [sg.Text(size=(40, 1), key='-OUTPUT-')],
+    [sg.Button('sim'), sg.Button('não')]
+]
+
+# Create the window
+window = sg.Window('Simulador de dado', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event = values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    # Output a message to the window
+    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] +
+                              "! Thanks for trying PySimpleGUI")
+
+# Finish up by removing from the screen
+window.close()
